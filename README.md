4519485c6fb3274fcb936b3b19dcdea14bfb8520
jjmedinas <juan.ms@outlook.com>
2019-04-26 00:08:18 -0500

    Add initial settings
    - Add initial settings
    - Overwriting spree layouts
    - Create a new view using a bootstrap template

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+Small project to try solidus ecommerce.
---
+++ app/controllers/custom_controller.rb
@@ -0,0 +1,6 @@
+class CustomController < ApplicationController
+
+  def test
+    # render json: { data: Spree::User.last }, status: 200
+  end
+end
